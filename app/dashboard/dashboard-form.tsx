c574f5d02d1396c11f0864dda38b0c35d49d0485
Michael Rosenberger <mail@michaelrosenberger.at>
2024-03-31 23:43:28 +0200

    update dashboard edit

---
+++ app/dashboard/dashboard-form.tsx
@@ -0,0 +1,157 @@
+"use client";
+import { useCallback, useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { type User } from "@supabase/supabase-js";
+import Avatar from "./avatar";
+
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+
+export default function DashboardForm({ user }: { user: User | null }) {
+  const supabase = createClient();
+  const [loading, setLoading] = useState(true);
+  const [fullname, setFullname] = useState<string | null>(null);
+  const [username, setUsername] = useState<string | null>(null);
+  const [website, setWebsite] = useState<string | null>(null);
+  const [avatar_url, setAvatarUrl] = useState<string | null>(null);
+
+  const getProfile = useCallback(async () => {
+    try {
+      setLoading(true);
+
+      const { data, error, status } = await supabase
+        .from("profiles")
+        .select(`full_name, username, website, avatar_url`)
+        .eq("id", user?.id)
+        .single();
+
+      if (error && status !== 406) {
+        console.log(error);
+        throw error;
+      }
+
+      if (data) {
+        setFullname(data.full_name);
+        setUsername(data.username);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      alert("Error loading user data!");
+    } finally {
+      setLoading(false);
+    }
+  }, [user, supabase]);
+
+  useEffect(() => {
+    getProfile();
+  }, [user, getProfile]);
+
+  async function updateProfile({
+    username,
+    website,
+    avatar_url,
+  }: {
+    username: string | null;
+    fullname: string | null;
+    website: string | null;
+    avatar_url: string | null;
+  }) {
+    try {
+      setLoading(true);
+
+      const { error } = await supabase.from("profiles").upsert({
+        id: user?.id as string,
+        full_name: fullname,
+        username,
+        website,
+        avatar_url,
+        updated_at: new Date().toISOString(),
+      });
+      if (error) throw error;
+      alert("Profile updated!");
+    } catch (error) {
+      alert("Error updating the data!");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="form-widget flex flex-row gap-8 w-full">
+      <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-6 space-y-4 basis-2/3">
+        <div className="space-y-2">
+          <Label htmlFor="email">Email</Label>
+          <Input
+            id="email"
+            name="email"
+            required
+            type="email"
+            disabled
+            value={user?.email}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="fullName">Full Name</Label>
+          <Input
+            id="fullName"
+            name="fullName"
+            type="text"
+            value={fullname || ""}
+            onChange={(e) => setFullname(e.target.value)}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="username">Username</Label>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value={username || ""}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="space-y-2">
+          <Label htmlFor="website">Website</Label>
+          <Input
+            id="website"
+            name="website"
+            type="url"
+            value={website || ""}
+            onChange={(e) => setWebsite(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <div className="rounded-lg border bg-card text-card-foreground shadow-sm p-6 space-y-4 basis-1/3">
+        <Avatar
+          uid={user?.id ?? null}
+          url={avatar_url}
+          size={150}
+          onUpload={(url) => {
+            setAvatarUrl(url);
+            updateProfile({ fullname, username, website, avatar_url: url });
+          }}
+        />
+        <div>
+          <Button
+            className="w-full"
+            onClick={() =>
+              updateProfile({ fullname, username, website, avatar_url })
+            }
+            disabled={loading}
+          >
+            {loading ? "Loading ..." : "Update"}
+          </Button>
+        </div>
+
+        <div>
+          <form action="/auth/signout" method="post">
+            <Button className="w-full" variant="outline">Logout</Button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation'
 import { cookies } from "next/headers";
+import DashboardForm from "./dashboard-form";
 
 import { Button } from '@/components/ui/button';
 
@@ -25,13 +26,11 @@ export default async function DashboardPage() {
 
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <div className="flex flex-col items-start gap-2">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+      <div className="flex flex-col items-start gap-4">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl mb-4">
           Hello <span className='text-primary'>{data.user.email}</span>
         </h1>
-        <form action={signOut} className="cursor-pointer">
-          <Button variant="outline">Logout</Button>
-      </form>
+        <DashboardForm user={data.user} />
       </div>
     </section>
   )
